ce0b25a42f40f4b448f629e37c11bba02ee6de86
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-05-22 18:23:39 -0500

    First CLI argument reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser; 
+
+#[derive(Parser)]
+struct Cli {
+	/// Input file path
+	path: std::path::PathBuf, 
+}
+
 fn main() {
-    println!("Hello, world!");
+	let args = Cli::parse(); 
+	
+	println!("path: {:?}", args.path)
 }
