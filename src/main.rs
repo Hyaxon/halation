5cfeec7ec504236d784a2360ba71da9d193af4d0
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-05-22 19:06:52 -0500

    Fixed variable name and missing bracket

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ struct Cli {
 	path: std::path::PathBuf, 
 }
 
-fn main() -> Result<() {
+fn main() -> Result<()> {
 	let args = Cli::parse(); 
 	
 	let input_img = image::open(&args.path).with_context(|| format!("could not read image `{}`", args.path.display()))?; 
 	
-	println!("path: {:?}", args.path)
+	println!("path: {:?}", args.path);
 
 	let (input_width, input_height) = input_img.dimensions();
-	println!("Loaded image: {}x{}", width, height);	
+	println!("Loaded image: {}x{}",input_width, input_height);	
 	
-	let pixel = img.get_pixel(0, 0); 
+	let pixel = input_img.get_pixel(0, 0); 
 	println!("Top-left pixel RGBA: {:?}", pixel); 
 
 	Ok(())
